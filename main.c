1b5a1d27262f487683994f0961e9bba3d65638b9
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-12-03 19:20:45 +0900

    header files

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
+#include "calc.h"
 #include <stdio.h>
 #include <stdlib.h> /* for atof() */
-
 #define MAXOP 100 /* max size of operand or operator */
-#define NUMBER '0' /* signal that a number was found */
-
-int getop(char[]);
-void push(double);
-double pop(void);
 
 /* reverse polish calculator */
 int main(void)
@@ -70,53 +65,4 @@ double pop(void)
         printf("error: stack empty\n");
         return 0.0;
     }
-}
-
-#include <ctype.h>
-
-int getch(void);
-void ungetch(int);
-
-/* getop: get next character or numeric operand */
-int getop(char s[])
-{
-    int i, c;
-
-    while ((s[0] = c = getch()) == ' ' || c == '\t' || c == '\n')
-        ;
-    s[1] = '\0';
-    if (!isdigit(c) && c != '.')
-        return c; /* not a number */
-
-    i = 0;
-    if (isdigit(c))
-        while (isdigit(s[++i] = c = getch()))
-            ;
-
-    if (c == '.') /* collect fraction part */
-        while (isdigit(s[++i] = c = getch()))
-            ;
-
-    s[i] = '\0';
-    if (c != EOF)
-        ungetch(c);
-    return NUMBER;
-}
-
-#define BUFSIZE 100
-
-char buf[BUFSIZE]; /* buffer for ungetch */
-int bufp = 0; /* next free position in buf */
-
-int getch(void) /* get a (possibly pushed-back) character */
-{
-    return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c) /* push character back on input */
-{
-    if (bufp >= BUFSIZE)
-        printf("ungetch: too many characters\n");
-    else
-        buf[bufp++] = c;
 }
